4ffe2f5ab20f263d37389e31415c62a0f14d633d
Detective Junior <b0855626503@gmail.com>
2021-05-02 21:29:51 +0700

    ปรับเวอชั่น 3.2.7

--- config/self-update.php
+++ config/self-update.php
@@ -22,7 +22,7 @@ return [
     |
     */
 
-    'version_installed' => env('SELF_UPDATER_VERSION_INSTALLED', '3.2.6'),
+    'version_installed' => env('SELF_UPDATER_VERSION_INSTALLED', '3.2.7'),
 
     /*
     |--------------------------------------------------------------------------
